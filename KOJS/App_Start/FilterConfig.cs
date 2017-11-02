d0f85c518faafd780af4951be84d20b770227a6a
TaoSun2016 <kevinsun9516@gmail.com>
2017-11-02 20:25:30 +1300

    Add project files.

---
+++ KOJS/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace KOJS
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
