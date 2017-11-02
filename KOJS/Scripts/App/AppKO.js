292d5abcf6f17469f58c2c3bac10290eed8bbbce
TaoSun2016 <kevinsun9516@gmail.com>
2017-11-02 20:51:11 +1300

    Add Model class Scaffolding Add js file

---
+++ KOJS/Scripts/App/AppKO.js
@@ -0,0 +1,17 @@
+﻿var viewModel = {
+    lookupCollection: ko.observableArray()
+};
+
+$(document).ready(funciton(){
+    $.ajax({
+        type: "GET",
+        url: "Lookups/GetIndex"
+    }).done(function (data) {
+        $(data).each(function (index, element) {
+            viewModel.lookupCollection.push(element);
+        });
+        ko.applyBindings(viewModel);
+    }).error(function (ex) {
+            alert("Error");
+    });
+});
