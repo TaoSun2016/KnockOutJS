e8ced23153a63aa8dd58fdd084e0d42bc00893ab
TaoSun2016 <kevinsun9516@gmail.com>
2017-11-03 15:32:20 +1300

    Add db

---
+++ KOJS/Models/MyTecBitsDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace KOJS.Models
+{
+    public class MyTecBitsDBContext : DbContext
+    {
+        public DbSet<MTB_Article> MTB_Articles { get; set; }
+}
+}
